394b51d4a5b535716b07931bcf85474fd587cfc4
edgar <spickerman.edgar@gmail.com>
2017-08-03 13:22:56 -0700

    rngGuessingGame changed to be more flexible

--- Courses/JavaScript Basics/First JavaScript Program/rngGuessingGame.js
+++ Courses/JavaScript Basics/First JavaScript Program/rngGuessingGame.js
@@ -1,11 +1,16 @@
 // JavaScript source code
 const upperBound = 6;
+var correctGuess = false; 
 const randomNumber = Math.floor(upperBound * Math.random()) + 1;
 
-const guess = prompt("I'm thinking of a random number between 1 and 6. What is it?")
+const guess = prompt("I'm thinking of a random number between 1 and " + upperBound + ". What is it?")
 
 if (parseInt(guess) === randomNumber) {
-    document.write("Way to go that's right.")
+    correctGuess = true;
+}
+
+if (correctGuess) {
+    document.write("Way to go that's right.");
 } else {
     doucment.write("Sorry, that's wrong. The number was " + randomNumber);
 }
