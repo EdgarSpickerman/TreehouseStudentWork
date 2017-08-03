151034b787f88ec185bb39ef2e91b7bc740b689b
edgar <spickerman.edgar@gmail.com>
2017-08-03 15:37:14 -0700

    added the crypto scrip.

---
+++ Courses/JavaScript Loops, Arrays, Objects/cryptocracy.js
@@ -0,0 +1,24 @@
+// JavaScript source code
+/*
+    Computer vs computer program. 
+        how many times it takes a computer to guess a randomly generated number.
+*/
+
+const upper = 10000;
+const randomNumber = getRandomNumber(upper);
+var guess; 
+let attempts = 0;
+
+function getRandomNumber(upper) {
+    return Math.floor(upper * Math.random) - 1;
+}
+
+while (guess !== randomNumber) {
+    guess = getRandomNumber(upper);
+    attempts += 1;
+}
+
+document.write("<p>The random number was " + randomNumber + ".</p><p> It took the computer " + attempts+ "attempts to guess correctly.</p>");
+
+print the random number and the number of times it took to guess it
+
