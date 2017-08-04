ec984b980d0e5dec8ebe44465d772f06ddef6453
edgar <spickerman.edgar@gmail.com>
2017-08-03 17:03:15 -0700

    commented out the nondry in favor of dry

--- Courses/JavaScript Loops, Arrays, Objects/nonDryScript.js
+++ Courses/JavaScript Loops, Arrays, Objects/nonDryScript.js
@@ -1,23 +1,97 @@
 // JavaScript source code
-var html = '';
 
-function randomRGB(){
-  return Math.floor(Math.random() * 256 )
-}
+/* Non Dry Code
+    var html = '';
+    var red;
+    var green;
+    var blue;
+    var rgbColor;
 
-function randomColor(){
-  var color = 'rgb('; 
-  color += randomRGB() + ',';
-  color += randomRGB() + ',';
-  color += randomRGB() + ')';
-  return color;
-}
-function print(message){
-  document.write(message);
-}
+    red = Math.floor(Math.random() * 256 );
+    green = Math.floor(Math.random() * 256 );
+    blue = Math.floor(Math.random() * 256 );
+    rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    html += '<div style="background-color:' + rgbColor + '"></div>';
 
-for(var i = 0; i < 10; i ++) {
-  html += '<div style="background-color:' +  randomColor() + '"></div>';
-}
+    red = Math.floor(Math.random() * 256 );
+    green = Math.floor(Math.random() * 256 );
+    blue = Math.floor(Math.random() * 256 );
+    rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    html += '<div style="background-color:' + rgbColor + '"></div>';
 
-print(html);
+    red = Math.floor(Math.random() * 256 );
+    green = Math.floor(Math.random() * 256 );
+    blue = Math.floor(Math.random() * 256 );
+    rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    html += '<div style="background-color:' + rgbColor + '"></div>';
+
+    red = Math.floor(Math.random() * 256 );
+    green = Math.floor(Math.random() * 256 );
+    blue = Math.floor(Math.random() * 256 );
+    rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    html += '<div style="background-color:' + rgbColor + '"></div>';
+
+    red = Math.floor(Math.random() * 256 );
+    green = Math.floor(Math.random() * 256 );
+    blue = Math.floor(Math.random() * 256 );
+    rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    html += '<div style="background-color:' + rgbColor + '"></div>';
+
+    red = Math.floor(Math.random() * 256 );
+    green = Math.floor(Math.random() * 256 );
+    blue = Math.floor(Math.random() * 256 );
+    rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    html += '<div style="background-color:' + rgbColor + '"></div>';
+
+    red = Math.floor(Math.random() * 256 );
+    green = Math.floor(Math.random() * 256 );
+    blue = Math.floor(Math.random() * 256 );
+    rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    html += '<div style="background-color:' + rgbColor + '"></div>';
+
+    red = Math.floor(Math.random() * 256 );
+    green = Math.floor(Math.random() * 256 );
+    blue = Math.floor(Math.random() * 256 );
+    rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    html += '<div style="background-color:' + rgbColor + '"></div>';
+
+    red = Math.floor(Math.random() * 256 );
+    green = Math.floor(Math.random() * 256 );
+    blue = Math.floor(Math.random() * 256 );
+    rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    html += '<div style="background-color:' + rgbColor + '"></div>';
+
+    red = Math.floor(Math.random() * 256 );
+    green = Math.floor(Math.random() * 256 );
+    blue = Math.floor(Math.random() * 256 );
+    rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    html += '<div style="background-color:' + rgbColor + '"></div>';
+
+    document.write(html);
+*/
+
+//Dry Code
+    var html = '';
+
+    function randomRGB() {
+        return Math.floor(Math.random() * 256);
+    }
+
+    function randomColor() {
+        let color = "rgb(";
+        color += randomRGB() + ",";
+        color += randomRGB() + ",";
+        color += randomRGB() + ")";
+        return color;
+    }
+
+    function print(message) {
+        document.write(message);
+    }
+
+    for (var i = 0; i < 10; i++) {
+        const color = randomColor(); 
+        html += '<div style="background-color:' + color + '"></div>';
+    }
+
+    print(html);
