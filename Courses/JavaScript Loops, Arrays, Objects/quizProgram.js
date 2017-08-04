2a74a520acf591dfcd564efb4854ff4eadc260c1
edgar <spickerman.edgar@gmail.com>
2017-08-03 19:15:15 -0700

    updaed the quiz program script from the previous course.

--- Courses/JavaScript Loops, Arrays, Objects/quizProgram.js
+++ Courses/JavaScript Loops, Arrays, Objects/quizProgram.js
@@ -1,5 +1,5 @@
 // JavaScript source code
-var quiz = [
+var questionAnswer = [
     ['What is 1+1', 2],
     ['What is 2+2', 4],
     ['What is 3+3', 6],
@@ -13,34 +13,34 @@ var wrongQuestion = [];
 
 var correct = 0;
 
-function print(list) {
+function print(message) {
+    document.write(message);
+}
+
+function htmlString(array) {
     var htmlOutut = '<ol>';
-    for (var i = 0; i < list.length; i++) {
-        htmlOutut += '<li>' + list[i] + '</li>';
+    for (var i = 0; i < array.length; i++) {
+        htmlOutut += '<li>' + array[i] + '</li>';
     }
     htmlOutut += '</ol>'
 
-    document.write(htmlOutut);
+    print(htmlOutut);
 }
 
-for (var i = 0; i < quiz.length; i++) {
-    var answer = parseInt(prompt(quiz[i][0]));
-    if (answer === quiz[i][1]) {
-        correctQuestion.push(quiz[i][0]);
+for (var i = 0; i < questionAnswer.length; i++) {
+    var answer = parseInt(prompt(questionAnswer[i][0]));
+    if (answer === questionAnswer[i][1]) {
+        correctQuestion.push(questionAnswer[i][0]);
         correct += 1;
     } else {
-        wrongQuestion.push(quiz[i][0]);    
+        wrongQuestion.push(questionAnswer[i][0]);    
     }
 }
 
 
 
-document.write('<h1>You got ' + correct + ' question(s) right.</h2>');
-document.write('You got these questions correct:');
-print(correctQuestion);
-document.write('You got these questions wrong:');
-print(wrongQuestion);
-<ol>
-    <li>question</li>
-
-</ol>
+print('<h1>You got ' + correct + ' question(s) right.</h2>');
+print('You got these questions correct:');
+htmlString(correctQuestion);
+print('You got these questions wrong:');
+htmlString(wrongQuestion);
